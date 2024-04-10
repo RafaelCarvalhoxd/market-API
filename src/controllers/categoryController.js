1e97fb3b609b44409961ec4f74b15d2e5a7fb156
Faelzxd <devrafaelcarvalho14@gmail.com>
2024-04-10 11:10:41 -0300

    feat: add name validation

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -14,12 +14,27 @@ exports.getAllCategories = async (req, res) => {
 
 exports.createCategories = async (req, res) => {
     try {
+        if (!req.body.name) {
+            res.status(422)
+                .json({succes: false, error: 'Name is required!'})
+        }
+
+        const existResult = await database.pool.query({
+            text: 'SELECT EXISTS (SELECT * FROM category WHERE name = $1)',
+            values: [req.body.name]
+        })
+
+        if (req.body.name) {
+            res.status(409).
+                json({succes: false, error: `Category ${req.body.name} already exists`})
+        }
+
         const result = await database.pool.query({
             text: 'INSERT INTO category (name) VALUES ($1) RETURNING *',
             values: [req.body.name]
         })
         res.status(201)
-            .json({ succes: true, data: result.rows });
+            .json({ succes: true, data: result.rows[0]});
     } catch (error) {
         console.log(error)
         res.status(500)
