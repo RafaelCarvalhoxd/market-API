01079c43e7db4dae57942d82b5b7c47d893edc0a
Faelzxd <devrafaelcarvalho14@gmail.com>
2024-04-10 08:06:18 -0300

    fix: type error

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const database = require('./src/services/database');
-const categoryRouter = require('./src/routes/categoryRoute')
-const productRouter = require('./src/routes/productRoute')
+const categoryRouter = require('./src/routes/categoryRoute');
+const productRouter = require('./src/routes/productRoute');
 const port = 3000;
 
 const app = express();
