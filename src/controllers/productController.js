7ae3f32ccfc5b104f48ba83b9ce580b834bf2508
Faelzxd <devrafaelcarvalho14@gmail.com>
2024-04-11 12:17:41 -0300

    feat: add put http method

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,5 @@
 const database = require('../services/database')
-const { validateName, validatePrice, validateCategoryId } = require('../utils/validationFields');
+const { validateName, validatePrice, validateCategoryId} = require('../utils/validationFields');
 
 exports.getAllProducts = async (req, res) => {
     try {
@@ -32,13 +32,12 @@ exports.createProduct = async (req, res) => {
         });
 
         if (!existsResult.rows[0].exists) {
-            return res.status(422)
-                .json({ error: 'Category id not found' });
+            return res.status(422).json({ error: 'Category id not found' });
         }
 
         const result = await database.pool.query({
             text: `
-                INSERT INTO product (name, description, price, currency, quantity, active, category_id)
+                INSERT INTO products (name, description, price, currency, quantity, active, category_id)
                 VALUES ($1, $2, $3, $4, $5, $6, $7)
                 RETURNING *
             `,
@@ -58,3 +57,48 @@ exports.createProduct = async (req, res) => {
         return res.status(500).json({ error: error.message });
     }
 };
+
+exports.uptadeProduct = async (req, res) => {
+    try {
+         if (!req.body.name || !req.body.description || !req.body.price || !req.body.currency || !req.body.quantity || !req.body.active || !req.body.category_id) {
+            return res.status(422).json({ error: 'All fields are required' })
+        }
+
+        const existsResult = await database.pool.query({
+            text: 'SELECT EXISTS (SELECT * FROM category WHERE id = $1)',
+            values: [req.body.category_id]
+        });
+
+        if (!existsResult.rows[0].exists) {
+            return res.status(422).json({ error: 'Category id not found' });
+        }
+
+        const result = await database.pool.query({
+            text: `
+            UPDATE products
+            SET name = $1, description = $2, price = $3, currency = $4, quantity = $5, active = $6, category_id = $7, updated_date = CURRENT_TIMESTAMP
+            WHERE id = $8
+            RETURNING *
+            `,
+            values: [
+                req.body.name,
+                req.body.description,
+                req.body.price,
+                req.body.currency,
+                req.body.quantity,
+                req.body.active,
+                req.body.category_id,
+                req.params.id
+            ]
+        })
+
+        if (result.rowCount == 0) {
+            return res.status(404).json({ error: 'Product not found' })
+        } 
+
+        return res.status(200).json(result.rows[0]);
+
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
