8b77d91de79eb2de3f4af0fce08978004192714f
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2024-04-12 17:23:04 -0300

    feat: add get products by categoryid

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -136,3 +136,21 @@ exports.getProductById = async (req, res) => {
         return res.status(500).json({ error: error.message });
     }
 }
+
+exports.getProductByCategory = async (req, res) => {
+    try {
+        const result = await database.pool.query({
+            text: 'SELECT * FROM products WHERE category_id = $1',
+            values: [req.params.categoryid]
+        })
+
+        if (result.rowCount == 0) {
+            return res.status(404).json({ error: 'Product not found' })
+        }
+
+        return res.status(200).json(result.rows)
+
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+};
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -4,6 +4,7 @@ const productController = require('../controllers/productController')
 
 router.get('/products', productController.getAllProducts)
 router.get('/products/:id', productController.getProductById)
+router.get('/category/:categoryid', productController.getProductByCategory)
 router.post('/products', productController.createProduct)
 router.put('/products/:id', productController.uptadeProduct)
 router.delete('/products/:id', productController.deleteProduct)
