5974f2d285fe7055129f2d3e6481c8d2ddecd3e4
Faelzxd <devrafaelcarvalho14@gmail.com>
2024-04-13 17:48:14 -0300

    fix get product by category route

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -4,7 +4,7 @@ const productController = require('../controllers/productController')
 
 router.get('/products', productController.getAllProducts)
 router.get('/products/:id', productController.getProductById)
-router.get('/categories/:categoryid', productController.getProductByCategory)
+router.get('/products/categories/:categoryid', productController.getProductByCategory)
 router.post('/products', productController.createProduct)
 router.put('/products/:id', productController.uptadeProduct)
 router.delete('/products/:id', productController.deleteProduct)
